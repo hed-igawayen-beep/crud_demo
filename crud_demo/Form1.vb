a6b58a96e7941495aac240dc74b938918fcb79a2
ivan john gawayen <hed-igawayen@smu.edu.ph>
2025-11-18 10:11:30 +0800

    button insert is working

--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -5,13 +5,30 @@ Public Class Form1
     Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
         conn = New MySqlConnection
         conn.ConnectionString = "server=localhost; userid=root; password=root; database=crud_demo_db;"
-
         Try
             conn.Open()
             MessageBox.Show("Connected")
         Catch ex As Exception
             MessageBox.Show(ex.Message)
             conn.Close()
+        End Try
+    End Sub
+
+    Private Sub ButtonInsert_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
+        Dim query As String = "INSERT INTO students_tbl (name, age, email) VALUES (@name, @age, @email)"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@name", TextBoxName.Text)
+                    cmd.Parameters.AddWithValue("@age", CInt(TextBoxAge.Text))
+                    cmd.Parameters.AddWithValue("@email", TextBoxEmail.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record insert succeessfully!")
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
         End Try
     End Sub
 End Class
